ade9f28f12ccb2a08b661903804910917ace7a8e
Lafaiete do Vale <lafa.vale@gmail.com>
2023-04-01 19:59:13 -0300

    Listing transactions

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelancer de website",
+          type: "deposit",
+          category: "Dev",
+          amount: 6000,
+          createdAt: new Date("2022-02-12 09:00:00"),
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 2000,
+          createdAt: new Date("2022-02-14 11:30:00"),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = "api";
 
